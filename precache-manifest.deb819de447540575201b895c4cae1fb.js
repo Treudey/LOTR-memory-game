f0f4366f6f6848964f4dc8a275b1dd9d725694c6
Treudey <alextreude@hotmail.com>
2019-06-12 20:03:34 -0400

    Updates

---
+++ precache-manifest.deb819de447540575201b895c4cae1fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648187113045da17b90061d5eaf03e6d",
+    "url": "/Treudey/LOTR-memory-game/index.html"
+  },
+  {
+    "revision": "81504211810c2eb32577",
+    "url": "/Treudey/LOTR-memory-game/static/css/main.507de7fb.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Treudey/LOTR-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "81504211810c2eb32577",
+    "url": "/Treudey/LOTR-memory-game/static/js/main.4ad315e2.chunk.js"
+  },
+  {
+    "revision": "4c454f4947278dc392e3",
+    "url": "/Treudey/LOTR-memory-game/static/js/runtime~main.cbfc5026.js"
+  }
+]);
