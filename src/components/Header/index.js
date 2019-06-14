47bc6cef82cd50caafc04c188e82a77fe2d9690a
Treudey <alextreude@hotmail.com>
2019-06-13 20:03:57 -0400

    Added first media queries

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 function Header() {
   return (
-    <div class="jumbotron jumbotron-fluid text-center">
-      <div class="container">
-        <h1 class="display-2">Lord of the Rings</h1>
-        <h2 class="display-3">Memory Game</h2>
-        <p class="lead">Click on a character to earn points, but don't click on someone more than once!</p>
+    <div className="jumbotron jumbotron-fluid text-center">
+      <div className="container">
+        <h1 className="display-2">Lord of the Rings</h1>
+        <h2 className="display-3">Memory Game</h2>
+        <p className="lead">Click on a character to earn points, but don't click on someone more than once!</p>
       </div>
     </div>
   );
